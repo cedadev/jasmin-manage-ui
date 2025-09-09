cb39f2e600f434ee0ad8075005b8bc94e206a342
Matthew Jones <matthew.j.jones@stfc.ac.uk>
2025-09-09 12:46:56 +0100

    Change column header and add text to explain

--- src/Components/AllProjects/List.js
+++ src/Components/AllProjects/List.js
@@ -77,7 +77,7 @@ const TableBody = ({ tableData, columns, rowVisible }) => {
 
 const TagList = ({projectsSummary, resources, tagData, consortium}) => {
     //Define columns
-    var columns = [ { label: 'Project', accessor: 'project', sortable: true }, { label: 'Consortium', accessor: 'consortium', sortable: true}, { label: 'Tags', accessor: 'tags', sortable: false }, { label: 'Earliest End Date', accessor: 'firsted', sortable: true } , { label: 'Furthest End Date', accessor: 'lasted', sortable: true }];
+    var columns = [ { label: 'Project', accessor: 'project', sortable: true }, { label: 'Consortium', accessor: 'consortium', sortable: true}, { label: 'Tags', accessor: 'tags', sortable: false }, { label: 'Earliest End Date', accessor: 'firsted', sortable: true } , { label: 'Latest End Date', accessor: 'lasted', sortable: true }];
     const dont_sum_columns = ['project', 'tags', 'consortium', 'firsted', 'lasted']; // this is needed because we try to sum all the fields
     var i = 0;
     while (i < Object.values(resources.data).length) {
@@ -206,6 +206,10 @@ const TagList = ({projectsSummary, resources, tagData, consortium}) => {
     return (<>
     <Col>  
         <p>View of all projects, including filters for tags and consortia. Some columns can be sorted.</p>
+        <p>Earliest and latest end dates for the project are taken from the requirements in the services of the project.
+            The earliest being the soonest expiry date for a requirement and the latest being the furthest away expiry date for a 
+            requirement.
+        </p>
         <Row>
             
             <Col>
